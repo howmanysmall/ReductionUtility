d452b796f299639b8e23710a3b24a8252386eb75
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-06-05 12:38:44 -0600

    congrats you barely work moonwave

--- src/Types.lua
+++ src/Types.lua
@@ -1,33 +1,10 @@
 --!optimize 2
 --!strict
 
---[=[
-	The types exported by this library.
-	@class Types
-]=]
-
---[=[
-	An action type.
-
-	You can use it as such:
-	```lua
-	local ReductionUtility = require("ReductionUtility")
-	type PassesString = ReductionUtility.Action<"PassesString"> & {Value: string}
-	```
-
-	@interface Action<Type = any>
-	.Type Type -- The type of the action.
-	@within Types
-]=]
 export type Action<Type = any> = {
 	type: Type,
 }
 
---[=[
-	A generic action type.
-	@type AnyAction {[string]: any, Type: any}
-	@within Types
-]=]
 export type AnyAction = {[string]: any} & Action
 
 export type ActionCreator<Type, Payload, Args...> = typeof(setmetatable(
